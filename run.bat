c46293baf0147fc421545fdae3bf475035134ef9
Steven Ellis <steven.ellis.d@gmail.com>
2021-05-18 07:43:25 -0400

    updated build/run scripts

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp target\BankingApp-1.0-SNAPSHOT.jar com.revature.banking.Driver
+java -Dlog4j2.configurationFile=src\main\resources\log4j2.xml -cp target\BankingApp-1.0-SNAPSHOT.jar com.revature.banking.Driver
