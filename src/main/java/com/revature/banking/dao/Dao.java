930035318b4f53378d88c0dad9d85a4d378f0c02
Steven Ellis <steven.ellis.d@gmail.com>
2021-05-20 18:25:42 -0400

    added remove method to dao interface

--- src/main/java/com/revature/banking/dao/Dao.java
+++ src/main/java/com/revature/banking/dao/Dao.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface Dao<T, T2> {
-    ArrayList<T> getAll();
+    List<T> getAll();
 
     T get(T2 id);
 
     void add(T newItem);
+
+    void remove(T itemToRemove);
 }
