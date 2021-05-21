3637ac97618af587632f70dde27235b9b4073631
Steven Ellis <steven.ellis.d@gmail.com>
2021-05-21 10:47:32 -0400

    switched transactionDao to database implementation

--- src/main/java/com/revature/banking/dao/TransactionDao.java
+++ src/main/java/com/revature/banking/dao/TransactionDao.java
@@ -1,12 +1,20 @@
 package com.revature.banking.dao;
 
 import com.revature.banking.models.Transaction;
+import com.revature.banking.services.ConnectionManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class TransactionDao implements Dao<Transaction, Integer>, Serializable {
     private ArrayList<Transaction> transactions;
+    static Logger logger = LogManager.getLogger(TransactionDao.class);
 
     public TransactionDao() {
         transactions = new ArrayList<Transaction>();
@@ -19,16 +27,34 @@ public class TransactionDao implements Dao<Transaction, Integer>, Serializable {
 
     @Override
     public Transaction get(Integer id) {
-        for (Transaction transaction: transactions) {
-            if (transaction.getId() == id) {
-                return transaction;
-            }
+        try (Connection conn = ConnectionManager.getConnection()) {
+            String sql = "select * from transactions where transaction_id = ?";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+
+            ResultSet result = statement.executeQuery();
+            result.next();
+
+            return new Transaction(result.getDouble("amount"),
+                    result.getInt("source_id"),
+                    result.getInt("destination_id"),
+                    result.getString("transaction_type"),
+                    result.getInt("transaction_id")) {
+            };
+        } catch (SQLException e) {
+            logger.error("error in database access when retrieving transaction by id");
+            return null;
         }
-        return null;
     }
 
     @Override
     public void add(Transaction transaction) {
         transactions.add(transaction);
+    }
+
+    @Override
+    public void remove(Transaction itemToRemove) {
+
     }
 }
--- src/main/java/com/revature/banking/models/Transaction.java
+++ src/main/java/com/revature/banking/models/Transaction.java
@@ -1,12 +1,12 @@
 package com.revature.banking.models;
 
 import java.io.Serializable;
-import java.util.UUID;
 
-public abstract class Transaction implements Serializable {
+public class Transaction implements Serializable {
     private final double amount;
-    private final UUID account;
-    private final int id;
+    private final Integer account;
+    private final Integer destination;
+    private final Integer id;
     private Type type;
     public enum Type {
         WITHDRAW,
@@ -14,17 +14,64 @@ public abstract class Transaction implements Serializable {
         TRANSFER
     }
 
-    public Transaction(double amount, UUID account, Type type) {
+    //constructors for transfer transaction, includes destination field
+    public Transaction(double amount, Integer account, Integer destination, Type type) {
         this.amount = amount;
         this.account = account;
+        this.destination = destination;
         this.type = type;
-        this.id = this.hashCode();
+        this.id = null;
     }
+    public Transaction(double amount, Integer account, Integer destination, String type) {
+        this.amount = amount;
+        this.account = account;
+        this.destination = destination;
+        this.type = Type.valueOf(type);
+        this.id = null;
+    }
+
+    public Transaction(double amount, Integer account, Integer destination, String type, Integer id) {
+        this.amount = amount;
+        this.account = account;
+        this.destination = destination;
+        this.type = Type.valueOf(type);
+        this.id = id;
+    }
+
+    //constructors for non-transfer transaction, does not include destination field
+    public Transaction(double amount, Integer account, Type type) {
+        this.amount = amount;
+        this.account = account;
+        this.destination = 0;
+        this.type = type;
+        this.id = null;
+    }
+    public Transaction(double amount, Integer account, String type) {
+        this.amount = amount;
+        this.account = account;
+        this.destination = 0;
+        this.type = Type.valueOf(type);
+        this.id = null;
+    }
+
+    public Transaction(double amount, Integer account, String type, Integer id) {
+        this.amount = amount;
+        this.account = account;
+        this.destination = 0;
+        this.type = Type.valueOf(type);
+        this.id = id;
+    }
+
+
 
     public double getAmount() { return amount; }
 
-    public UUID getAccount() {
+    public int getAccount() {
         return account;
+    }
+
+    public int getDestination() {
+        return destination;
     }
 
     public int getId() {
